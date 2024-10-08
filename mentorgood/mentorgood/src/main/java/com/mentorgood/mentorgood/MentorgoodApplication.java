5ee07d67bb9c1a39cfe681bfab26995ed059efb6
Dung Pham <anhdungp0304@gmail.com>
2024-10-08 14:45:20 +0700

    initialize project's BE

---
+++ mentorgood/mentorgood/src/main/java/com/mentorgood/mentorgood/MentorgoodApplication.java
@@ -0,0 +1,14 @@
+package com.mentorgood.mentorgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorgoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorgoodApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ mentorgood/mentorgood/src/test/java/com/mentorgood/mentorgood/MentorgoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentorgood.mentorgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorgoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
