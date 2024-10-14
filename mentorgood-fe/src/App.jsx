c975f81d114e98acc2deddac0b2312e9755278b0
Dung Pham <anhdungp0304@gmail.com>
2024-10-14 19:25:56 +0700

    re-initialize FE pj (2)

--- mentorgood-fe/src/App.jsx
+++ mentorgood-fe/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Input } from "@nextui-org/react";
+import { useState } from "react";
+import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
+import { EyeFilledIcon } from "./EyeFilledIcon";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="max-w-lg w-full bg-white p-10 rounded shadow">
+        <p className="mb-7 font-medium text-3xl">Login</p>
+        <Input
+          type="email"
+          label="Email"
+          variant="bordered"
+          defaultValue="junior2nextui.org"
+          isInvalid={false}
+          errorMessage="Please enter a valid email"
+          className="w-full"
+        />
+        <Input
+          label="Password"
+          variant="bordered"
+          placeholder="Enter your password"
+          endContent={
+            <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+              {isVisible ? (
+                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+              ) : (
+                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+              )}
+            </button>
+          }
+          type={isVisible ? "text" : "password"}
+          className="w-full"
+        />
+        <Button className="mt-4" color="primary">
+          Login
+        </Button>  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
 
-export default App
+  );
+}
